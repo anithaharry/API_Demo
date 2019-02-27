7ccc37dbb33c583dd798b70d27d2be7d0e5e3ec4
Anitha Harry <anitha.harry@loudclear.com.au>
2019-02-28 09:46:30 +1100

    initial commit

---
+++ API/APITests/Controllers/TransformControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using API.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API.Models;
+
+namespace API.Controllers.Tests
+{
+    [TestClass()]
+    public class TransformControllerTests
+    {
+        [TestMethod()]
+        public void PostTest()
+        {
+            var controller = new TransformController();
+            RawInfo rawInfo = new RawInfo { Name = "Anita", Value = 123.45M };
+            var response = controller.Post(rawInfo);
+            Response converted = response.Data as Response;
+            Assert.IsTrue(converted.ConvertedInfoValues.ConvertedValue.Equals("ONE HUNDRED AND TWENTY-THREE DOLLARS AND FORTY-FIVE CENTS"));
+            Assert.IsTrue(converted.ConvertedInfoValues.Name.Equals(rawInfo.Name));
+            Assert.IsNotNull(response.Data);
+        }
+    }
+}
